31decf50e81e168ef80135ec8c2b221e4b982b2d
412-39-yanisa <56870@yupparaj.ac.th>
2026-08-18 10:53:41 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -8,18 +8,18 @@ st.write("กรอกข้อมูลน้ำหนักและส่ว�
 weight = st.number_input("กรอกนํ้าหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
 height_cm = st.number_input("กรอกส่วนวสูงของคุณ (เซนติเมตร):", min_value=1.0, value=1.0)
 
-  if st.button("คำนวณค่า BMI"):
-    height_m = height_cm / 100
-    bmi = weight / (height_m ** 2)
+if st.button("คำนวณค่า BMI"):
+  height_m = height_cm / 100
+  bmi = weight / (height_m ** 2)
 
-    st.write("---")
-    st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
+  st.write("---")
+  st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
 
-    if bmi < 18.5:
+  if bmi < 18.5:
       st.warning("คุณมีนํ้าหนักน้อยกว่าเกณฑ์ (ผอม)")
-    elif 18.5 <= bmi < 23.0
+  elif 18.5 <= bmi < 23.0
       st.success("คุณมีนํ้่าหนักอยู่ในเกณฑ์ปกติ (สุขภาพดี)")
-    elif 23.0 <= bmi < 25.0:
+   elif 23.0 <= bmi < 25.0:
       st.info("คุณเริ่มมีนํ้าหนักเกินเกณฑ์ (ท้วม)")
   else:
       st.error("คุณอยู่ในเกณฑ์อ้วน ควรระวังเรื่องสุขภาพและออกกำลังกาย")
